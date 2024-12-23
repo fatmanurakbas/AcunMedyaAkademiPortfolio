37308c7d98dbd6ddedb39f51d3cc8dde3be4bf45
Fatma Nur <135032308+fatmanurakbas@users.noreply.github.com>
2024-12-23 13:52:24 +0300

    PartialView Eklendi

--- AcunMedyaAkademiPortfolio/Controllers/AdminLayoutController.cs
+++ AcunMedyaAkademiPortfolio/Controllers/AdminLayoutController.cs
@@ -13,5 +13,22 @@ namespace AcunMedyaAkademiPortfolio.Controllers
         {
             return View();
         }
+
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SidebarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
     }
 }
